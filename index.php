c6fff31126b32d0ad579149a26d8c01b7dfdea62
Labinopper <Labinopper@googlemail.com>
2021-02-16 22:30:47 +0000

    Initial setup

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
 <body>
     <div id="login-form">
     <form method="post" name="login">
-        <h1 class="login-title">Login here</h1>
+        <h1 class="login-title">Login here!</h1>
         <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
         <input type="password" class="login-input" name="password" placeholder="Password"/><br>
         <input type="submit" value="Login" name="submit" class="login-button"/>
