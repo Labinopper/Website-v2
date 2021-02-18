f4b3f99828e9b7855c1c560eb0a1726c3b2e89e9
Labinopper <Labinopper@googlemail.com>
2021-02-18 22:40:40 +0000

    .

--- gameoverview.php
+++ gameoverview.php
@@ -97,28 +97,6 @@
         <input type="Text" name="newcategory">
         <input type="submit" value="Add new Category" name="AddCategorySubmit">
     </form>
-    <?php
-    
-    $query = "SELECT * FROM `GameCategory` WHERE game_id=$id";
-    $result = mysqli_query($conn, $query);
-    $row = mysqli_num_rows($result);
-    if ($result->num_rows > 0) {
-        $x = 0;
-        while($row = $result->fetch_assoc()) {
-            $x++;
-            $categoryname = stripslashes($row['categoryname']);    // removes backslashes
-            $categoryname = mysqli_real_escape_string($conn, $categoryname);
-            $categoryid = stripslashes($row['categoryid']);    // removes backslashes
-            $categoryid = mysqli_real_escape_string($conn, $categoryid);
-            echo '<form id="GOAddCategory'.$x.'" method="POST">
-            <input type="hidden" name="newcategory'.$x.'id" value="'.$row["id"].'">
-            <input type="Text" name="newcategory'.$x.'">
-            <input type="submit" value="Add new '.$categoryname.'" name="Add'.$x.'Submit">
-            </form>';
-        }
-    }
-    
-    ?>
     
     </body>
     </html>
