fb8f959228ed6b107218f7f8555adade72097e23
Labinopper <Labinopper@googlemail.com>
2021-02-18 20:19:03 +0000

    Edit Menu

--- addplay.php
+++ addplay.php
@@ -92,17 +92,8 @@
   <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <div id="Banner">
-<center><p id="Title">Welcome <?php echo $_SESSION['username'] ?>!</p></center></div>
-
-    <div id="Menu">
-        <a href="logout.php">Logout</a>
-        <a href="dashboard.php">Homepage</a>
-        <a href="addgame.php">Games</a>
-        <a href="addplay.php">Plays</a>
-        <a href="purchases.php">Purchases</a>
-        <a href="kickstarters2.php">Add Kickstarter</a>
-    </div> 
+<?php include('header.php'); ?>
+ 
     
         <?php
     
