bf4dd635de6a0ae6b6d485505a2fbbecb2a8cef6
Labinopper <Labinopper@googlemail.com>
2021-02-18 20:14:33 +0000

    Edit header

---
+++ header.php
@@ -0,0 +1,10 @@
+    <div id="Banner">
+<center><p id="Title">Welcome <?php echo $_SESSION['username'] ?>!</p></center></div>
+
+    <div id="Menu">
+        <a href="logout.php">Logout</a>
+        <a href="dashboard.php">Homepage</a>
+        <a href="addgame.php">Games</a>
+        <a href="addplay.php">Plays</a>
+        <a href="purchases.php">Purchases</a>
+    </div>
