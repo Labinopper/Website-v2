3aeff2aa6ce7e416d8b55ffd5f7a185fba82d0e6
Labinopper <Labinopper@googlemail.com>
2021-02-18 20:52:55 +0000

    Fix header

--- header.php
+++ header.php
@@ -1,6 +1,3 @@
-
-
-
 <style>
 body {
   font-family: Arial, Helvetica, sans-serif;
@@ -9,6 +6,8 @@ body {
 .navbar {
   overflow: hidden;
   background-color: #333;
+  border-style: line;
+  border-radius: 5px;
 }
 
 .navbar a {
@@ -92,15 +91,5 @@ body {
     <div class="dropdown-content">
         <a href="strava\index.php">Dashboard</a>
         <a href="strava\oauth-flow.php">oAuth Test</a>
+        </div>
 </div>
-
-<!---    <div id="Banner">
-<center><p id="Title">Welcome <?php echo $_SESSION['username'] ?>!</p></center></div>
-
-    <div id="Menu">
-        <a href="logout.php">Logout</a>
-        <a href="dashboard.php">Homepage</a>
-        <a href="addgame.php">Games</a>
-        <a href="addplay.php">Plays</a>
-        <a href="purchases.php">Purchases</a>
-    </div> -->
