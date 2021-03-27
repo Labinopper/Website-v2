15134de9ce49b13fc09cd94f01986bcf5d26f6a6
Andy Hannibal <labinopper@googlemail.com>
2021-03-27 16:24:18 +0000

    Revert ""
    This reverts commit 69da20251de0baa5ad5cca44b03b583ebed8ab6c.

--- kickstarters2.php
+++ kickstarters2.php
@@ -50,19 +50,23 @@
 <?php include('header.php');
  
     if(isset($_GET['id'])) {
-        echo '<div id="AddNewKS">';
-        echo '<form class="form" method="post" name="newks">';
-        echo '<h1 class ="addpurchase"><center>New Kickstarter</center></h1>';
-        echo '<select name = "Games" class="ksinput">';
+        ?>
+        <div id="AddNewKS">
+            <form class="form" method="post" name="newks">
+        <h1 class ="addpurchase"><center>New Kickstarter</center></h1>
+        <select name = "Games" class="ksinput">
+            <?php 
             
             $sql2 = "select * from Games left join Kickstarters on Kickstarters.game_id = Games.id where Kickstarters.id = $id";
             $result2 = $conn->query($sql2);
+            
                 while($row2 = $result2->fetch_assoc()) {
                     echo "<option value = \"". $row2['id'] . "\">" . $row2['name'] . "</option>";
 
-        echo '</select>';
-        echo '<input type="comment" class="ksinput" name="pm" placeholder="Pledge Manager">';
-        echo '<span id="ksdate1">PM Opens:<input type="date" class="newks-date" value="'.$row2['pmopens'].'" name="pmopendate" min="2020-01-01"></span>';
+            ?>
+        </select>
+        <input type="comment" class="ksinput" name="pm" placeholder="Pledge Manager">
+        <?php echo '<span id="ksdate1">PM Opens:<input type="date" class="newks-date" value="'.$row2['pmopens'].'" name="pmopendate" min="2020-01-01"></span>';
         echo '<span id="ksdate2">PM Closes:<input type="date" class="newks-date" value="'.$row2['pmcloses'].'" name="pmclosedate" min="2020-01-01"></span>';
         echo '<span id="ksdate3">Delivery Date:<input type="date" class="newks-date" value="'.$row2['delivery_date'].'" name="deliverydate" min="2020-01-01"></span>';
         if($row2["progress"] === "A") {
@@ -143,10 +147,12 @@
             echo '</select></center></td>';
         }
 
-        echo '<br><br><br>';
-        echo '<input type="submit" value="Add Game" name="submit" class="newks-submit"/>';
-        echo '</form>';
-        echo '</div>';
+        ?>
+        <br><br><br>
+        <input type="submit" value="Add Game" name="submit" class="newks-submit"/>
+        </form>
+        </div>
+        <?php
     } else {
         ?>
         <div id="AddNewKS">
